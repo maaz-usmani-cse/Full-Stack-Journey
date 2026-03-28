1d4b226cef4574b9faecc7d75ff2441e07d9cff9
maaz usmani <maazusmani15@gmail.com>
2026-03-28 22:07:06 +0530

    Python:Function Pattern Practice Set_07(Question)

---
+++ 02_Python/04_Function/2026-03-28-Function_Set_07.py
@@ -0,0 +1,38 @@
+#PATTERN
+
+'Left angle Triangle with *'
+# def left_triangle(n):
+#     for i in range(1,n+1):
+#         print('*'*i)
+
+# user=int(input("enter your number"))
+# print(left_triangle(user))
+
+
+
+'Right angle triangle with *'
+
+# def right_triangle(n):
+#     for i in range(1,n+1):
+#         print(' '*(n-i),"*"*i)
+
+
+
+# user=int(input("enter your number"))
+# print(right_triangle(user))
+
+
+
+
+'left angle triangle with even number'
+# def left_triangle_even(n):
+#     x=2
+#     for i in range(1,n+1):
+#         for j in range(1,i+1):
+#             print(x,end=' ')
+#             x=x+2
+#         print()
+        
+
+# user=int(input("enter your number"))
+# print(left_triangle_even(user))
