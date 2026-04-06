d4ac688875dccd97e9f877e887375d960ee23bf4
maaz usmani <maazusmani15@gmail.com>
2026-04-06 23:57:30 +0530

    python: Function Practice Set_13(Questions) & Added Javascript:Day_01_Set

---
+++ 02_Python/04_Function/2026-04-06-Function_Set_13.py
@@ -0,0 +1,32 @@
+'. Write a program to find the intersection of two list.'
+# def intersection_of_list(l1,l2):
+#     l=[]
+#     for i in l1:
+#         for j in l2:
+#             if i not in l:
+#                  if i==j:
+#                      l.append(i)
+#     return l
+
+# l1=eval(input("enter your list"))
+# l2=eval(input("enter your list"))
+# result=intersection_of_list(l1,l2)
+# print(result)
+
+
+
+
+'. Implement a program to shift list in . Left Shift'
+# def left_shift(l):
+#     temp=l[0]
+#     for i in range(len(l)-1):
+#         l[0]=l[i+1]
+#     l[-1]=temp
+#     return l
+
+# l=eval(input("enetr your list"))
+# result=left_shift(l)
+# print(result)
+
+
+
---
+++ 04_Javascript/Day_01/Basic_Set_01.js
@@ -0,0 +1,58 @@
+'JAVA SCRIPT ?'
+
+
+/*
+java script definitions:-
+Simple Definition
+JavaScript ek aisi scripting language hai jo web browsers mein chalti hai. Ye static (ruki hui) websites ko interactive banati hai—jaise ki button click karne par menu khulna, animations dikhana, ya bina page refresh kiye data update hona.
+
+JavaScript Kya Kya Kar Sakta Hai? (Key Features)
+Interactivity: Aapne dekha hoga jab aap kisi image par mouse le jate hain toh wo badi ho jati hai, ya "Like" button dabane par turant count badh jata hai. Ye sab JS ka kamaal hai.
+
+Form Validation: Agar aapne login karte waqt galat email dala hai, toh wo turant red color mein warning dikhata hai.
+
+Dynamic Content: Facebook ya Instagram par jab aap niche scroll karte hain aur naye posts apne aap aa jate hain, wahan JavaScript kaam kar rahi hoti hai.
+
+Beyond Browser: Aaj kal JavaScript sirf websites tak simit nahi hai. Isse mobile apps, desktop softwares, aur server-side (Node.js) programming bhi ki jati hai.
+
+
+
+
+Ek Chota Sa Udaharan (Example)
+Maan lijiye aap chahte hain ki jab koi button dabaye, toh ek message dikhe:
+
+JavaScript
+function helloDosto() {
+  alert("Hello! JavaScript seekhna asaan hai.");
+}
+
+*/
+
+
+
+'LINKS JAVA SCRIPT TO HTML'
+/*
+javA SCRIPT KO HTML K SATH DO TRH S LINK KR SKTE HAI
+1.INTERNAL.           SYNTAX-->>  (IN HTML PAGE <script>
+                                         code...
+                                    </script>)
+2.EXTERNAL ()
+
+
+
+
+
+
+##OUTPUT STATEMENT
+
+##3 out statement hota hai java script m
+1.console.log
+2.alert
+3.document.write
+*/
+
+
+
+// console.log("maaz usmani")
+// alert("request pending")
+// document.write("first class")
