583a388d7dcebaacf1dbf4769e0894b529753928
Maaz Usmani <maazusmani15@gmail.com>
2026-08-18 23:37:32 +0530

    Python Function Practice Set_136(Questions)

---
+++ 02_Python/04_Function/2026-08-18-Function-Set_136.py
@@ -0,0 +1,122 @@
+'''
+Ek string "Learn Python Fast" ke saare spaces ko underscore ("_") se replace karein.
+
+'''
+
+# def replace_space_with_underscore(s):
+#     new_word=s.replace(' ','_')
+#     return new_word
+
+# user=input("enter your word")
+# result=replace_space_with_underscore(user)
+# print(result)
+
+
+
+'''
+Remove Commas: String "1,000,000" se saare commas (,) hata kar "1000000" banayein.
+
+'''
+# def remove_commas(s):
+#     new_word=s.replace(',', '')
+#     return new_word
+
+# user=input('enter yoyr word')
+# result=remove_commas(user)
+# print(result)
+
+
+
+'''
+Word Swap: Sentence "I love Java" mein "Java" ko "Python" se replace karein.
+'''
+# def word_swap(s):
+#     new_word=s.replace('Java','python')
+#     return new_word
+
+# user=input('enter your word')
+# result=word_swap(user)
+# print(result)
+
+
+
+
+'''
+Vowel Hide: String "elephant" ke saare 'e' ko '*' se replace karein.
+'''
+# def vowel_hide(s):
+#     new_word=s.replace('e','*')
+#     return new_word
+
+# user=input("enter your word")
+# result=vowel_hide(user)
+# print(result)
+
+
+
+'Double to Single Space: String "Hello  world!  How  are  you?" ke sabhi double spaces ("  ") ko single space (" ") se replace karein.'
+
+# def double_to_single(s):
+#     new_word=s.replace('  ',' ')
+#     return new_word
+
+# user=input("enter your word")
+# result=double_to_single(user)
+# print(result)
+
+
+'First Two Replacements: String "one, two, three, four, one, two" mein pehle do "one" ko "zero" se replace karein.'
+# def replace_one_with_zero(s):
+#     new_word=s.replace('one','zero')
+#     return new_word
+
+# user=input('enter your word')
+# result=replace_one_with_zero(user)
+# print(result)
+
+
+'Date Format Fix: Date "2026/08/18" mein sirf pehla "/" replace karke "-" karein (Output: "2026-08/18").'
+# def fix_date_format(s):
+#     new_word=s.replace('/','-',1)
+#     return new_word
+
+# user=input('enter your word')
+# result=fix_date_format(user)
+# print(result)
+
+
+'''
+Limited Masking: Number string "555-555-5555" ke pehle do "555" ko "XXX" karein.
+'''
+# def limited_masking(s):
+#     new_word=s.replace('555','XXX',2)
+#     return new_word
+
+# user=input('enter your word')
+# result=limited_masking(user)
+# print(result)
+
+
+'String "bad word, bad guy, bad day" ke pehle do "bad" ko "good" karein.'
+# def first_two_word_replace(s):
+#     new_word=s.replace('bad','good',2)
+#     return new_word
+
+# user=input("ente your word")
+# result=first_two_word_replace(user)
+# print(result)
+
+
+
+'''
+Phone Number Cleaner: String "+91 (123) 456-7890" se bracket (, ), hyphen -, aur space sabhi ko chain karke hatayein.
+'''
+# def phone_number_cleaner(s):
+#     cleaned=s.replace(' ','').replace('-','').replace('(','').replace(')','')
+#     return cleaned
+
+# user=input("enter your word")
+# result=phone_number_cleaner(user)
+# print(result)
+
+
